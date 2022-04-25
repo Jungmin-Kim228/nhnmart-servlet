ce4ae6180051f213fa33b4776f34e6e59c15b0e2
dgb980228 <dgb980228@naver.com>
2022-04-25 23:52:18 +0900

    refactoring methods

--- nhnmart-servlet/src/main/java/com/nhnacademy/nhnmart/cartServlet.java
+++ nhnmart-servlet/src/main/java/com/nhnacademy/nhnmart/cartServlet.java
@@ -20,26 +20,11 @@ public class cartServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException {
 
-        this.foodStand = (ArrayList<Food>) getServletContext().getAttribute("foodStand");
-        int[] foodAmounts = new int[4];
         int idx = 0;
-        for (String foodValue : req.getParameterValues("food")) {
-            foodAmounts[idx] = Optional.of(Integer.parseInt(foodValue)).orElse(0);
-            if (foodAmounts[idx] > this.foodStand.get(idx).getAmount() || foodAmounts[idx] < 0) {
-                resp.setStatus(417);
-                resp.sendError(417, "Out of amount");
-                return;
-            }
-            idx++;
-        }
+        this.foodStand = (ArrayList<Food>) getServletContext().getAttribute("foodStand");
+        int[] foodAmounts = checkAmountValid(req, resp, idx);
 
-        idx = 0;
-        for (Food food : this.foodStand) {
-            this.basket.add(new Food(food.getName(), food.getPrice(), foodAmounts[idx]));
-            food.setAmount(food.getAmount()-foodAmounts[idx]);
-            this.totalPrice += foodAmounts[idx] * food.getPrice();
-            idx++;
-        }
+        initBasketAndTotal(foodAmounts, idx);
         getServletContext().setAttribute("foodStand", foodStand);
 
         resp.sendRedirect("/cart");
@@ -70,6 +55,29 @@ public class cartServlet extends HttpServlet {
         out.println();
     }
 
+    private int[] checkAmountValid(HttpServletRequest req, HttpServletResponse resp, int idx) throws IOException {
+        String[] received = req.getParameterValues("food");
+        int[] foodAmounts = new int[received.length];
 
+        for (String foodValue : received) {
+            foodAmounts[idx] = Optional.of(Integer.parseInt(foodValue)).orElse(0);
+            if (foodAmounts[idx] > this.foodStand.get(idx).getAmount() || foodAmounts[idx] < 0) {
+                resp.setStatus(417);
+                resp.sendError(417, "Out of amount");
+                return null;
+            }
+            idx++;
+        }
+        return foodAmounts;
+    }
+
+    private void initBasketAndTotal(int[] foodAmounts, int idx) {
+        for (Food food : this.foodStand) {
+            this.basket.add(new Food(food.getName(), food.getPrice(), foodAmounts[idx]));
+            food.setAmount(food.getAmount()-foodAmounts[idx]);
+            this.totalPrice += foodAmounts[idx] * food.getPrice();
+            idx++;
+        }
+    }
 
 }
